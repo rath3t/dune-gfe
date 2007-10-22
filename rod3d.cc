14fb84d442b7067c3ce827c1c0291c1fea1f3828
Oliver Sander <sander@igpm.rwth-aachen.de>
2007-10-22 17:30:46 +0000

    bugfix: use the fd Hessian matrix for error norm computation
    [[Imported from SVN: r1717]]

--- rod3d.cc
+++ rod3d.cc
@@ -183,7 +183,7 @@ int main (int argc, char *argv[]) try
     MatrixIndexSet indices(exactSolution.size(), exactSolution.size());
     rodAssembler.getNeighborsPerVertex(indices);
     indices.exportIdx(hessian);
-    rodAssembler.assembleMatrix(exactSolution, hessian);
+    rodAssembler.assembleMatrixFD(exactSolution, hessian);
 
 
     double error = std::numeric_limits<double>::max();
