1bec916637c366440a395687cb0f0ce1fe75e6b1
Oliver Sander <sander@igpm.rwth-aachen.de>
2007-10-22 16:27:44 +0000

    gradient computation by fd approximation is removed
    [[Imported from SVN: r1712]]

--- src/rodassembler.hh
+++ src/rodassembler.hh
@@ -262,9 +262,6 @@ public:
         void assembleGradient(const std::vector<Configuration>& sol,
                               Dune::BlockVector<Dune::FieldVector<double, blocksize> >& grad) const;
 
-        void assembleGradientFD(const std::vector<Configuration>& sol,
-                                Dune::BlockVector<Dune::FieldVector<double, blocksize> >& grad) const;
-
         /** \brief Compute the energy of a deformation state */
         double computeEnergy(const std::vector<Configuration>& sol) const;
 
