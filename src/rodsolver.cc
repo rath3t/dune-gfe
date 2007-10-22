534dbceeccdaa65d41b0f704def17fbdfd35a1bc
Oliver Sander <sander@igpm.rwth-aachen.de>
2007-10-22 16:24:31 +0000

    compute gradient analytically and not be fd approximation
    [[Imported from SVN: r1711]]

--- src/rodsolver.cc
+++ src/rodsolver.cc
@@ -230,8 +230,7 @@ void RodSolver<GridType>::solve()
 
         if (this->verbosity_ == FULL)
             std::cout << "Rod energy: " <<rodAssembler_->computeEnergy(x_) << std::endl;
-        //rodAssembler_->assembleGradient(x_, rhs);
-        rodAssembler_->assembleGradientFD(x_, rhs);
+        rodAssembler_->assembleGradient(x_, rhs);
         //rodAssembler_->assembleMatrix(x_, *hessianMatrix_);
         rodAssembler_->assembleMatrixFD(x_, *hessianMatrix_);
 
