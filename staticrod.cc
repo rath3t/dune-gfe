f50180ffb0456077cf4863f47f614548a72aab56
Oliver Sander <sander@igpm.rwth-aachen.de>
2008-03-04 16:55:57 +0000

    new path to ag-common
    [[Imported from SVN: r2012]]

--- staticrod.cc
+++ staticrod.cc
@@ -1,24 +1,24 @@
 #include <config.h>
 
-//#define DUNE_EXPRESSIONTEMPLATES
+#include <dune/common/bitfield.hh>
+#include <dune/common/configparser.hh>
+
 #include <dune/grid/onedgrid.hh>
 
 #include <dune/istl/io.hh>
 
-#include "../common/boundarypatch.hh"
-#include <dune/common/bitfield.hh>
+#include <dune/ag-common/boundarypatch.hh>
 
 #include "src/planarrodassembler.hh"
-#include "../common/projectedblockgsstep.hh"
+#include <dune/ag-common/projectedblockgsstep.hh>
 #include "../contact/src/contactmmgstep.hh"
 
-#include "../common/iterativesolver.hh"
+#include <dune/ag-common/iterativesolver.hh>
 
-#include "../common/geomestimator.hh"
-#include "../common/energynorm.hh"
+#include <dune/ag-common/geomestimator.hh>
+#include <dune/ag-common/energynorm.hh>
 #include "src/rodwriter.hh"
 
-#include <dune/common/configparser.hh>
 
 // Number of degrees of freedom: 
 // 3 (x, y, theta) for a planar rod
