9a3742c4893faf522c04515b812af10225549a83
Oliver Sander <sander@igpm.rwth-aachen.de>
2008-03-04 17:30:31 +0000

    work with the latest executable
    [[Imported from SVN: r2019]]

--- dirneu_testsuite/testsuite.sh
+++ dirneu_testsuite/testsuite.sh
@@ -6,8 +6,8 @@ set -e
 runComputation(){
 
 RESULTPATH=`pwd`/$1/
-PARAMETERFILE=${RESULTPATH}${1}".parset"
-LOGFILE=${RESULTPATH}${1}".log"
+PARAMETERFILE=${RESULTPATH}${1}"_"${3}".parset"
+LOGFILE=${RESULTPATH}${1}"_"${3}".log"
 
 #echo $RESULTPATH
 
@@ -22,13 +22,10 @@ cat > "${PARAMETERFILE}" <<EOF
 numLevels = $2
 
 # Tolerance of the Dirichlet-Neumann solver
-ddTolerance = 1e-12
+ddTolerance = 1e-9
 
 # Max number of Dirichlet-Neumann steps
-maxDirichletNeumannSteps = 500
-
-# Damping for the Dirichlet-Neumann solver
-damping = $3
+maxDirichletNeumannSteps = 30
 
 # Tolerance of the trust-region solver for the rod problem
 trTolerance = 1e-12
@@ -36,8 +33,8 @@ trTolerance = 1e-12
 # Max number of steps of the trust region solver
 maxTrustRegionSteps = 20
 
-# Initial trust-region radius
-initialTrustRegionRadius = 1
+# Verbosity of the trust-region solver
+trVerbosity = 0
 
 # Number of multigrid iterations per trust-region step
 numIt = 30
@@ -58,33 +55,132 @@ baseIt = 100
 mgTolerance = 1e-13
 
 # Tolerance of the base grid solver
-baseTolerance = 1e-8
+baseTolerance = 1e-13
+
+# Initial trust-region radius
+initialTrustRegionRadius = 1
+
+# Damping
+damping = $3
 
 # Measure convergence
-instrumented = 1
+instrumented = 0
 
 ############################
 #   Problem specifications
 ############################
 
-path = /home/haile/sander/data/multicoupling/simplecoupling/
-gridFile = hexarod.grid
-dirichletNodes = hexarod.dn
-dirichletValues = hexarod.nodv
-interfaceNodes  = hexarod.ifn
+#path = /home/haile/sander/data/multicoupling/simplecoupling/
+#gridFile = hexarod.grid
+#dirichletNodes = hexarod.dn
+#dirichletValues = hexarod.nodv
+#interfaceNodes  = hexarod.ifn
 
-# Number of elements of the rod grid
-numRodBaseElements = 5
+#numRodBaseElements = 4
+
+## Cross-section area
+#rodA = 0.0625
+
+## Geometric moments (here: square of edge length 0.25)
+#rodJ1 = 0.0013021
+#rodJ2 = 0.0013021
+
+## Material parameters
+#rodE  = 1e6
+#rodNu = 0.3
+
+#E     = 1e6
+#nu    = 0.3
+
+#rodRestEndPoint0X = 0.125
+#rodRestEndPoint0Y = 0.125
+#rodRestEndPoint0Z = 1
+#rodRestEndPoint1X = 0.125
+#rodRestEndPoint1Y = 0.125
+#rodRestEndPoint1Z = 2
+
+## Dirichlet values
+#dirichletValueX = 0.125
+#dirichletValueY = 0.125
+#dirichletValueZ = 2.5
+
+#dirichletAxisX = 0
+#dirichletAxisY = 0
+#dirichletAxisZ = 1
+#dirichletAngle = 0
+
+
+########################################################
+#path = /home/haile/sander/data/multicoupling/simplecoupling/
+#gridFile = cube_5x5x5.grid
+#dirichletNodes = cube_5x5x5.dn
+#dirichletValues = cube_5x5x5.nodv
+#interfaceNodes  = cube_5x5x5.ifn
+
+#numRodBaseElements = 5
+
+# Cross-section area
+#rodA = 1
+
+# Geometric moments (here: square of edge length one)
+#rod J1 = 0.0833333
+#rod J2 = 0.0833333
+
+# Material parameters
+#rodE  = 2.5e5
+#rodNu = 0.3
+
+## Dirichlet values
+#dirichletValueX = 0.5
+#dirichletValueY = 1.5
+#dirichletValueZ = 10
+
+#dirichletAxisX = 0
+#dirichletAxisY = 0
+#dirichletAxisZ = 1
+#dirichletAngle = 90
+
+########################################################
+path = /home/haile/sander/data/multicoupling/simplecoupling/
+gridFile = cube_4x4x4.grid
+dirichletNodes = cube_4x4x4.dn
+dirichletValues = cube_4x4x4.nodv
+interfaceNodes  = cube_4x4x4.ifn
+
+numRodBaseElements = 4
+
+# Cross-section area
+rodA = 0.0625
+
+# Geometric moments (here: square of edge length 0.25)
+rodJ1 = 0.0013021
+rodJ2 = 0.0013021
+
+# Material parameters
+rodE  = 1e6
+rodNu = 0.3
+E     = 1e6
+nu    = 0.3
+
+rodRestEndPoint0X = 0.625
+rodRestEndPoint0Y = 0.625
+rodRestEndPoint0Z = 1
+#rodRestEndPoint1X = 0.625
+#rodRestEndPoint1Y = 0.625
+#rodRestEndPoint1Z = 2
+rodRestEndPoint1X = 0.625
+rodRestEndPoint1Y = -0.082
+rodRestEndPoint1Z = 1.707
 
 # Dirichlet values
-dirichletValueX = 0.5
-dirichletValueY = 0.6
-dirichletValueZ = 10
+dirichletValueX = 0.625
+dirichletValueY = 0.875
+dirichletValueZ = 2
 
 dirichletAxisX = 0
 dirichletAxisY = 0
 dirichletAxisZ = 1
-dirichletAngle = 10
+dirichletAngle = 90
 
 # Where to write the results
 resultPath = $RESULTPATH
@@ -103,23 +199,23 @@ EOF
 # run problems
 
 
-#for level in 2; do
-for level in 1 2 3 4 5 6; do
+for level in 4; do
+#for level in 1 2 3 4; do
 
-    LEVELDIR=${level}"levels"
+    LEVELDIR=${level}"levels_rot"
     
     if test -e ${LEVELDIR}/convrates; then
         rm ${LEVELDIR}/convrates
     fi
 
-    #for damping in 0.05; do
-    for damping in 0.001 0.002 0.005 0.01 0.02 0.03 0.04 0.05; do
+    for damping in 0.6; do
+#    for damping in 0.1 0.2 0.3 0.4 0.5 0.6 0.7 0.8 0.9 1.0 1.1 1.2 1.3; do
 
         echo "Computing on "${level}" levels with damping factor "${damping}
         runComputation $LEVELDIR $level $damping
 
         # Append convergence rate of this run to overall list for this level
-        cat convrate >> ${LEVELDIR}/convrates
+        #cat convrate >> ${LEVELDIR}/convrates
 
     done
 
