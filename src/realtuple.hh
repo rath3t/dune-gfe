3882e9216701ed083c14e9129c1be69ed7eba24e
Oliver Sander <sander@igpm.rwth-aachen.de>
2010-02-10 21:11:59 +0000

    implement the distance metric
    [[Imported from SVN: r5526]]

--- src/realtuple.hh
+++ src/realtuple.hh
@@ -17,6 +17,8 @@ public:
 
     typedef Dune::FieldVector<double,N> EmbeddedTangentVector;
 
+    typedef Dune::FieldVector<double,N> TangentVector;
+
     /** \brief Default constructor */
     RealTuple()
     {}
@@ -26,6 +28,17 @@ public:
     {
         data_.assign(v);
     }
+
+    /** \brief Geodesic distance between two points 
+
+    Simply the Euclidean distance */
+    static double distance(const RealTuple& a, const RealTuple& b) {
+        double result = 0;
+        for (int i=0; i<N; i++)
+            result += (a.data_[0] - b.data_[0]) * (a.data_[0] - b.data_[0]);
+        return std::sqrt(result);
+    }
+
     
     /** \brief Write LocalKey object to output stream */
     friend std::ostream& operator<< (std::ostream& s, const RealTuple& realTuple)
