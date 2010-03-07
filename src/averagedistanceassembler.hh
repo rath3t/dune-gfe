8d14410317a11e4061ad2b2270bb70f5f92742b7
Oliver Sander <sander@igpm.rwth-aachen.de>
2010-03-07 09:01:51 +0000

    remove old code that uses the derivative of the distance functions (instead of the derivative of the squared distance function)
    [[Imported from SVN: r5666]]

--- src/averagedistanceassembler.hh
+++ src/averagedistanceassembler.hh
@@ -33,16 +33,9 @@ public:
                           typename TargetSpace::EmbeddedTangentVector& gradient) const
     {
         gradient = 0;
-#if 0  // This old code uses the derivative of dist(,), which is (frequently, at least)
-        // not differentiable at 0
-        for (size_t i=0; i<coefficients_.size(); i++)
-            gradient.axpy(weights_[i]*TargetSpace::distance(coefficients_[i], x),
-                          TargetSpace::derivativeOfDistanceWRTSecondArgument(coefficients_[i], x));
-#else
         for (size_t i=0; i<coefficients_.size(); i++)
             gradient.axpy(0.5*weights_[i], 
                           TargetSpace::derivativeOfDistanceSquaredWRTSecondArgument(coefficients_[i], x));
-#endif
     }
 
     void assembleHessianApproximation(const TargetSpace& x,
