a51c7de1bb246a3b5ea1b062dd1fb48681d8a0cf
Oliver Sander <sander@igpm.rwth-aachen.de>
2010-11-19 10:06:20 +0000

    slightly better variable name
    [[Imported from SVN: r6545]]

--- dune/gfe/localgeodesicfefunction.hh
+++ dune/gfe/localgeodesicfefunction.hh
@@ -317,11 +317,11 @@ evaluateDerivativeOfGradientWRTCoefficient(const Dune::FieldVector<ctype, dim>& 
     for (size_t i=0; i<result.size(); i++) {
         
         //
-        Tensor3<double,embeddedDim,embeddedDim,embeddedDim> dcDqF
+        Tensor3<double,embeddedDim,embeddedDim,embeddedDim> dvDqF
            =  TargetSpace::thirdDerivativeOfDistanceSquaredWRTFirst1AndSecond2Argument(coefficients_[i], q);
     
     
-        result[i] = dFdqPseudoInv * ( dcDqF[i] * dFdqPseudoInv * dFdw - dpDwF[i]) * B;   
+        result[i] = dFdqPseudoInv * ( dvDqF[i] * dFdqPseudoInv * dFdw - dpDwF[i]) * B;   
      
     }
     
