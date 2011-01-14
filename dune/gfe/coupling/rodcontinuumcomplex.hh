1474025383e5ab54a22c306e563e004f4b0bb47c
Oliver Sander <sander@igpm.rwth-aachen.de>
2011-01-14 11:48:17 +0000

    also store Dirichlet boundaries and Dirichlet values
    [[Imported from SVN: r6753]]

--- dune/gfe/coupling/rodcontinuumcomplex.hh
+++ dune/gfe/coupling/rodcontinuumcomplex.hh
@@ -6,18 +6,46 @@
 
 #include <dune/common/shared_ptr.hh>
 
+#include <dune/istl/bvector.hh>
+
+#include <dune/gfe/rigidbodymotion.hh>
+
 /** \brief A set of rods and a set of continua, all coupled to each other
  */
 template <class RodGrid, class ContinuumGrid>
 class RodContinuumComplex
 {
+    typedef std::vector<RigidBodyMotion<3> > RodConfiguration;
+    
+    typedef Dune::BlockVector<Dune::FieldVector<double,3> > ContinuumConfiguration;
+    
 public:
+
+    /////////////////////////////////////////////////////////////////////
+    //  Data concerning the individual rod problems
+    /////////////////////////////////////////////////////////////////////
 
     /** \brief The set of rods, accessible by name (string) */
     std::map<std::string, Dune::shared_ptr<RodGrid> > rodGrids_;
     
-    /** \brief The set of rods, accessible by name (string) */
+    /** \brief A Dirichlet boundary for each rod */
+    std::map<std::string, LeafBoundaryPatch<RodGrid> > rodDirichletBoundaries_;
+    
+    /** \brief The Dirichlet values for each rod */
+    std::map<std::string, RodConfiguration> rodDirichletValues_;
+    
+    /////////////////////////////////////////////////////////////////////
+    //  Data concerning the individual continuum problems
+    /////////////////////////////////////////////////////////////////////
+
+    /** \brief The set of continua, accessible by name (string) */
     std::map<std::string, Dune::shared_ptr<ContinuumGrid> > continuumGrids_;
+    
+    /** \brief A Dirichlet boundary for each continuum */
+    std::map<std::string, LeafBoundaryPatch<ContinuumGrid> > continuumDirichletBoundaries_;
+
+    /** \brief The Dirichlet values for each continuum */
+    std::map<std::string, ContinuumConfiguration> continuumDirichletValues_;
     
 };
 
