cec05bfad24b58e4703858402b8f09adb88fbb66
Oliver Sander <sander@igpm.rwth-aachen.de>
2011-10-12 15:56:40 +0000

    small documentation improvement
    [[Imported from SVN: r7887]]

--- dune/gfe/cosseratenergystiffness.hh
+++ dune/gfe/cosseratenergystiffness.hh
@@ -82,6 +82,8 @@ public:  // for testing
         // This second derivative is almost given by the method getFirstDerivativesOfDirectors.
         // However, since the directors of a given unit quaternion are the _columns_ of the
         // corresponding orthogonal matrix, we need to invert the i and j indices
+        //
+        // So, if I am not mistaken, DR[i][j][k] contains \partial M_ij / \partial k
         Tensor3<double,3 , 3, 4> dd_dq;
         value.q.getFirstDerivativesOfDirectors(dd_dq);
         
