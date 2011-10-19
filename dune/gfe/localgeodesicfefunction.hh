890304a20523411b3d66c43228520f1d69d5dc4c
Jonathan Youett <youett@mi.fu-berlin.de>
2011-10-19 14:25:30 +0000

    also rename the forward declaration
    [[Imported from SVN: r7961]]

--- dune/gfe/localgeodesicfefunction.hh
+++ dune/gfe/localgeodesicfefunction.hh
@@ -14,8 +14,8 @@
 #include <dune/gfe/linearalgebra.hh>
 
 // forward declaration
-template <int dim, class ctype, class LocalFiniteElement, class TargetSpace>
-class LocalGFETestFunction;
+template <class LocalFiniteElement, class TargetSpace>
+class LocalGfeTestFunctionBasis;
 
 /** \brief A function defined by simplicial geodesic interpolation 
            from the reference element to a Riemannian manifold.
