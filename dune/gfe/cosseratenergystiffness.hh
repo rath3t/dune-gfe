e794c50d19b82e6929fcead2d6936a963908ab92
Oliver Sander <sander@igpm.rwth-aachen.de>
2012-03-15 07:22:15 +0000

    Remove a pointless 'sym' and corresponding comment.
    Because trace(sym(A)) == trace(A)
    
    [[Imported from SVN: r8568]]

--- dune/gfe/cosseratenergystiffness.hh
+++ dune/gfe/cosseratenergystiffness.hh
@@ -318,8 +318,7 @@ public:
             
         return mu_ * sym(RT_DR3).frobenius_norm2()
                + mu_c_ * skew(RT_DR3).frobenius_norm2()
-               /** \todo Is this sym correct?  It is in the paper, but not in the notes */
-               + mu_*lambda_/(2*mu_+lambda_) * traceSquared(sym(RT_DR3));
+               + mu_*lambda_/(2*mu_+lambda_) * traceSquared(RT_DR3);
     }
 
     ///////////////////////////////////////////////////////////////////////////////////////
