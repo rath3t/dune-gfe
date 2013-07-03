345239ba869617cbd5db6a27f8023d239d4a5836
Oliver Sander <sander@igpm.rwth-aachen.de>
2013-07-03 16:10:52 +0000

    add missing header
    [[Imported from SVN: r9288]]

--- test/valuefactory.hh
+++ test/valuefactory.hh
@@ -7,6 +7,7 @@
 #include <dune/gfe/rotation.hh>
 #include <dune/gfe/rigidbodymotion.hh>
 #include <dune/gfe/orthogonalmatrix.hh>
+#include <dune/gfe/hyperbolichalfspacepoint.hh>
 
 /** \brief A class that creates sets of values of various types, to be used in unit tests
  * 
