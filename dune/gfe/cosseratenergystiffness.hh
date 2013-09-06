0f3274b5023af6b79870d38ec85a9880932c3c6b
Oliver Sander <sander@igpm.rwth-aachen.de>
2013-09-06 11:03:06 +0000

    Bugfix: by error parameter L_c_ lost a square in a recent patch
    This patch brings it back.
    
    [[Imported from SVN: r9477]]

--- dune/gfe/cosseratenergystiffness.hh
+++ dune/gfe/cosseratenergystiffness.hh
@@ -365,9 +365,9 @@ public:
     RT curvatureEnergy(const Tensor3<field_type,3,3,3>& DR) const
     {
 #ifdef DONT_USE_CURL
-        return mu_ * std::pow(L_c_ * DR.frobenius_norm2(),q_/2.0);
+        return mu_ * std::pow(L_c_ * L_c_ * DR.frobenius_norm2(),q_/2.0);
 #else
-        return mu_ * std::pow(L_c_ * curl(DR).frobenius_norm2(),q_/2.0);
+        return mu_ * std::pow(L_c_ * L_c_ * curl(DR).frobenius_norm2(),q_/2.0);
 #endif
     }
 
