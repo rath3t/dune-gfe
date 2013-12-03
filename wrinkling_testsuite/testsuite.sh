aacd1c58fa227286cf3a5992e89be83f3b9ffcd8
Oliver Sander <sander@igpm.rwth-aachen.de>
2013-12-03 13:06:33 +0000

    Make the shell thickness always be twice L_c
    Patrizio told me on the phone today that this is the way it should be.
    
    [[Imported from SVN: r9557]]

--- wrinkling_testsuite/testsuite.sh
+++ wrinkling_testsuite/testsuite.sh
@@ -8,6 +8,9 @@ numLevels=$1
 mu_c=$2
 L_c=$3
 
+# Shell thickness is always twice the L_c parameter
+thickness=$(perl -e "print ${L_c} * 2")
+
 RESULTPATH=`pwd`/cosserat_wrinkling_${mu_c}_${L_c}_${numLevels}/
 LOGFILE="${RESULTPATH}/cosserat_wrinkling_${mu_c}_${L_c}_${numLevels}.log"
 
@@ -33,7 +36,7 @@ fi
 #  run the actual simulation
 #################################################
 
-../cosserat-continuum -numLevels ${numLevels} -materialParameters.L_c ${L_c} -resultPath ${RESULTPATH} | tee ${LOGFILE}
+../cosserat-continuum -numLevels ${numLevels} -materialParameters.L_c ${L_c} -materialParameters.thickness ${thickness} -resultPath ${RESULTPATH} | tee ${LOGFILE}
 
 }
 
@@ -42,10 +45,10 @@ MAXPROCS=4
 
 mu_c=0
 
-for numLevels in 1 2 3 4 5 6; do
+for numLevels in 2 3 4 5 6; do
 
-    for L_c in 0.1 0.01 0.001 0.0001 1e-5 1e-6 1e-7 1e-8; do
- 
+    for L_c in 1e-1 1e-2 1e-3 1e-4 1e-5 1e-6 1e-7 1e-8; do
+
         # Do one simulation run
         runComputation $numLevels $mu_c $L_c  &
 
