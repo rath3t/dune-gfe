a8b2a5ecf2252b9f9f8396257937de670e6871f6
Oliver Sander <sander@igpm.rwth-aachen.de>
2014-09-30 17:48:47 +0000

    A class to read Cosserat configurations from a VTK file
    [[Imported from SVN: r9897]]

---
+++ dune/gfe/cosseratvtkreader.hh
@@ -0,0 +1,44 @@
+#ifndef DUNE_GFE_COSSERATVTKREADER_HH
+#define DUNE_GFE_COSSERATVTKREADER_HH
+
+#include <dune/gfe/rigidbodymotion.hh>
+
+namespace Dune
+{
+  namespace GFE
+  {
+
+    /** \brief Read configurations of Cosserat models from VTK files into memory */
+    class CosseratVTKReader
+    {
+    public:
+
+      static void read(std::vector<RigidBodyMotion<double,3> >& configuration,
+                       const std::string& filename)
+      {
+        VTKFile vtkFile;
+        vtkFile.read(filename);
+
+        configuration.resize(vtkFile.points_.size());
+
+        for (size_t i=0; i<configuration.size(); i++)
+        {
+          configuration[i].r = vtkFile.points_[i];
+
+          FieldMatrix<double,3,3> R;
+          for (int j=0; j<3; j++)
+            for (int k=0; k<3; k++)
+              R[j][k] = vtkFile.directors_[j][i][k];
+
+          configuration[i].q.set(R);
+        }
+
+      }
+
+    };
+
+  }
+
+}
+
+#endif
