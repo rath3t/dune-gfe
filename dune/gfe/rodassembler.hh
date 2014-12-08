253d33082eacf1e71e3fdfbe74e8b3d9277585d8
Oliver Sander <sander@igpm.rwth-aachen.de>
2014-12-08 15:22:12 +0000

    Remove trailing whitespace
    [[Imported from SVN: r9979]]

--- dune/gfe/rodassembler.hh
+++ dune/gfe/rodassembler.hh
@@ -17,7 +17,7 @@
 template <class GridView, int spaceDim>
 class RodAssembler
 {
-    dune_static_assert(spaceDim==2 || spaceDim==3, 
+    dune_static_assert(spaceDim==2 || spaceDim==3,
                        "You can only instantiate the class RodAssembler for 2d and 3d spaces");
 };
 
@@ -26,7 +26,7 @@ class RodAssembler
 template <class GridView>
 class RodAssembler<GridView,3> : public GeodesicFEAssembler<P1NodalBasis<GridView>, RigidBodyMotion<double,3> >
 {
-        
+
     //typedef typename GridType::template Codim<0>::Entity EntityType;
     //typedef typename GridType::template Codim<0>::EntityPointer EntityPointer;
     typedef typename GridView::template Codim<0>::Iterator ElementIterator;
@@ -38,16 +38,16 @@ class RodAssembler<GridView,3> : public GeodesicFEAssembler<P1NodalBasis<GridVie
 
         //! Each block is x, y, theta in 2d, T (R^3 \times SO(3)) in 3d
         enum { blocksize = 6 };
-        
+
         //!
         typedef Dune::FieldMatrix<double, blocksize, blocksize> MatrixBlock;
-        
+
 public:
         //! ???
     RodAssembler(const GridView &gridView,
-                 RodLocalStiffness<GridView,double>* localStiffness) 
+                 RodLocalStiffness<GridView,double>* localStiffness)
         : GeodesicFEAssembler<P1NodalBasis<GridView>, RigidBodyMotion<double,3> >(gridView,localStiffness)
-        { 
+        {
             std::vector<RigidBodyMotion<double,3> > referenceConfiguration(gridView.size(gridDim));
 
             typename GridView::template Codim<gridDim>::Iterator it    = gridView.template begin<gridDim>();
@@ -73,13 +73,13 @@ public:
         void assembleGradient(const std::vector<RigidBodyMotion<double,3> >& sol,
                               Dune::BlockVector<Dune::FieldVector<double, blocksize> >& grad) const;
 
-        void getStrain(const std::vector<RigidBodyMotion<double,3> >& sol, 
+        void getStrain(const std::vector<RigidBodyMotion<double,3> >& sol,
                        Dune::BlockVector<Dune::FieldVector<double, blocksize> >& strain) const;
 
-        void getStress(const std::vector<RigidBodyMotion<double,3> >& sol, 
+        void getStress(const std::vector<RigidBodyMotion<double,3> >& sol,
                        Dune::BlockVector<Dune::FieldVector<double, blocksize> >& stress) const;
 
-        /** \brief Return resultant force across boundary in canonical coordinates 
+        /** \brief Return resultant force across boundary in canonical coordinates
 
         \note Linear run-time in the size of the grid */
         template <class PatchGridView>
@@ -94,63 +94,63 @@ public:
 template <class GridView>
 class RodAssembler<GridView,2> : public GeodesicFEAssembler<P1NodalBasis<GridView>, RigidBodyMotion<double,2> >
 {
-    
+
     typedef typename GridView::template Codim<0>::Entity EntityType;
     typedef typename GridView::template Codim<0>::Iterator ElementIterator;
-    
+
     //! Dimension of the grid.  This needs to be one!
     enum { gridDim = GridView::dimension };
-    
+
     enum { elementOrder = 1};
-    
+
     //! Each block is x, y, theta
     enum { blocksize = 3 };
-    
+
     //!
     typedef Dune::FieldMatrix<double, blocksize, blocksize> MatrixBlock;
-    
+
     /** \brief Material constants */
     double B;
     double A1;
     double A3;
-    
+
 public:
-    
+
     //! ???
-    RodAssembler(const GridView &gridView) 
+    RodAssembler(const GridView &gridView)
         : GeodesicFEAssembler<P1NodalBasis<GridView>, RigidBodyMotion<double,2> >(gridView,NULL)
-    { 
+    {
         B = 1;
         A1 = 1;
         A3 = 1;
     }
-    
+
     ~RodAssembler() {}
-    
+
     void setParameters(double b, double a1, double a3) {
         B  = b;
         A1 = a1;
         A3 = a3;
     }
-    
+
     /** \brief Assemble the tangent stiffness matrix and the right hand side
      */
     void assembleMatrix(const std::vector<RigidBodyMotion<double,2> >& sol,
                         Dune::BCRSMatrix<MatrixBlock>& matrix);
-    
+
     void assembleGradient(const std::vector<RigidBodyMotion<double,2> >& sol,
                           Dune::BlockVector<Dune::FieldVector<double, blocksize> >& grad) const;
-    
+
     /** \brief Compute the energy of a deformation state */
     double computeEnergy(const std::vector<RigidBodyMotion<double,2> >& sol) const;
-    
+
 protected:
-    
+
     /** \brief Compute the element tangent stiffness matrix  */
-    void getLocalMatrix( EntityType &entity, 
-                         const std::vector<RigidBodyMotion<double,2> >& localSolution, 
+    void getLocalMatrix( EntityType &entity,
+                         const std::vector<RigidBodyMotion<double,2> >& localSolution,
                          Dune::Matrix<MatrixBlock>& mat) const;
-    
+
 }; // end class
 
 #include "rodassembler.cc"
