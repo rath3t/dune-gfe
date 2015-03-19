ede1bfb1b70e3401def231490a0766acc420b964
Oliver Sander <sander@igpm.rwth-aachen.de>
2015-03-19 18:58:52 +0000

    Add temporary HAVE_CSTDDEF, to make IPOpt happy
    Seems to be a bug in IPOpt, or in our build system
    
    [[Imported from SVN: r10086]]

--- src/cosserat-continuum.cc
+++ src/cosserat-continuum.cc
@@ -4,6 +4,9 @@
 
 #include <fenv.h>
 
+// Hack to make IPOpt happy.  No idea why the build system doesn't properly handle this
+#define HAVE_CSTDDEF
+
 // Includes for the ADOL-C automatic differentiation library
 // Need to come before (almost) all others.
 #include <adolc/adouble.h>
--- src/finite-strain-elasticity.cc
+++ src/finite-strain-elasticity.cc
@@ -1,5 +1,8 @@
 #include <config.h>
 
+// Hack to make IPOpt happy.  No idea why the build system doesn't properly handle this
+#define HAVE_CSTDDEF
+
 // Includes for the ADOL-C automatic differentiation library
 // Need to come before (almost) all others.
 #include <adolc/adouble.h>
