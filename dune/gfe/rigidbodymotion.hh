aba4fd6aa46c121b269b1815c5a45331e760b138
Oliver Sander <oliver.sander@tu-dresden.de>
2016-02-21 06:35:59 +0100

    Replace 'Dune::is_same' by 'std::is_same'

--- dune/gfe/rigidbodymotion.hh
+++ dune/gfe/rigidbodymotion.hh
@@ -88,7 +88,7 @@ public:
             result.r[i] = p.r[i] + v[i];
 
         // Add rotational correction
-        typedef typename std::conditional<Dune::is_same<TVector,TangentVector>::value,
+        typedef typename std::conditional<std::is_same<TVector,TangentVector>::value,
                                           typename Rotation<ctype,N>::TangentVector,
                                           typename Rotation<ctype,N>::EmbeddedTangentVector>::type RotationTangentVector;
         RotationTangentVector qCorr;
