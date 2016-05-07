4278dc68a36829c06102b5d0bef3ab68fd682c93
Oliver Sander <oliver.sander@tu-dresden.de>
2016-05-07 07:56:20 +0200

    Make output logging work in the Python testsuite
    By using the same mechanism as in the shell script:  piping everything through
    the 'tee' program.  This may not be the most elegant way from a Python standpoint,
    but it seems to work.
    
    Since now my Python testsuite seems to do everything the shell version did,
    this commit also removes the shell version.

--- harmonicmaps-skyrmions-eoc-testsuite/testsuite.py
+++ harmonicmaps-skyrmions-eoc-testsuite/testsuite.py
@@ -14,9 +14,11 @@ for order in range(1,4):
 
         ##  run the actual simulation
         executable = "./harmonicmaps-" + str(order)
-        p = subprocess.Popen([executable, "harmonicmaps-skyrmions-hexagon.parset", "-numLevels", str(numLevels)])
+        #p = subprocess.Popen([executable, "harmonicmaps-skyrmions-hexagon.parset", "-numLevels", str(numLevels)])
+        p = subprocess.Popen(executable + " harmonicmaps-skyrmions-hexagon.parset -numLevels " + str(numLevels) + " | tee " + LOGFILE, shell=True)
         processList.append(p)
 
+    # Wait for all simulation subprocesses before proceeding to the error measurement step
     exit_codes = [p.wait() for p in processList]
 
     subprocess.call(["echo", "Now measuring errors"])
@@ -26,11 +28,18 @@ for order in range(1,4):
         # Measure the discretization errors against the solution on the finest grid
         LOGFILE = "./compute-disc-error_" + str(order) + "_" + str(numLevels) + ".log"
 
-        subprocess.Popen(["../build-cmake/src/compute-disc-error", "compute-disc-error-skyrmions-hexagon.parset",
-                                        "-order", str(order),
-                                        "-numLevels", str(numLevels),
-                                        "-numReferenceLevels", str(maxLevel),
-                                        "-simulationData", "harmonicmaps-result-" + str(order) + "-" + str(numLevels) + ".data",
-                                        "-referenceData", "harmonicmaps-result-" + str(order) + "-" + str(maxLevel) + ".data"])
+        #subprocess.Popen(["../build-cmake/src/compute-disc-error", "compute-disc-error-skyrmions-hexagon.parset",
+                                        #"-order", str(order),
+                                        #"-numLevels", str(numLevels),
+                                        #"-numReferenceLevels", str(maxLevel),
+                                        #"-simulationData", "harmonicmaps-result-" + str(order) + "-" + str(numLevels) + ".data",
+                                        #"-referenceData", "harmonicmaps-result-" + str(order) + "-" + str(maxLevel) + ".data"])
+        subprocess.Popen("../build-cmake/src/compute-disc-error compute-disc-error-skyrmions-hexagon.parset"
+                                      + " -order " + str(order)
+                                      + " -numLevels " + str(numLevels)
+                                      + " -numReferenceLevels " + str(maxLevel)
+                                      + " -simulationData harmonicmaps-result-" + str(order) + "-" + str(numLevels) + ".data"
+                                      + " -referenceData harmonicmaps-result-" + str(order) + "-" + str(maxLevel) + ".data"
+                                      + " | tee " + LOGFILE, shell=True)
 
 
