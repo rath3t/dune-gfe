403d9a2b2701ba7814ca32024a78525404e58c60
Oliver Sander <oliver.sander@tu-dresden.de>
2016-05-08 22:28:59 +0200

    Write result as VTK _vector_ data (not scalar data)
    Otherwise, the ParaView 'glyph' filter won't recognize the direction fields.

--- src/harmonicmaps.cc
+++ src/harmonicmaps.cc
@@ -254,7 +254,7 @@ int main (int argc, char *argv[]) try
     auto xFunction = Dune::Functions::makeDiscreteGlobalBasisFunction<TargetSpace::CoordinateType>(feBasis,TypeTree::hybridTreePath(),xEmbedded);
 
     VTKWriter<GridType::LeafGridView> vtkWriter(grid->leafGridView());
-    vtkWriter.addVertexData(xFunction, VTK::FieldInfo("orientation", VTK::FieldInfo::Type::scalar, xEmbedded[0].size()));
+    vtkWriter.addVertexData(xFunction, VTK::FieldInfo("orientation", VTK::FieldInfo::Type::vector, xEmbedded[0].size()));
     vtkWriter.write(resultPath + "_" + energy + "_result");
 
     // Write the corresponding coefficient vector: verbatim in binary, to be completely lossless
