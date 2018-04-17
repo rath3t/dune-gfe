b406fbb5b3e29da264294374455d110b5cdcc47a
Oliver Sander <oliver.sander@tu-dresden.de>
2018-04-17 13:13:06 +0200

    Dirichlet boundary values for the cantilever problem

---
+++ problems/cantilever-dirichlet-values.py
@@ -0,0 +1,17 @@
+import math
+
+class DirichletValues:
+    def __init__(self, homotopyParameter):
+        self.homotopyParameter = homotopyParameter
+
+    def deformation(self, x):
+        # Dirichlet b.c. simply clamp the shell in the reference configuration
+        out = [x[0], x[1], 0]
+
+        return out
+
+
+    def orientation(self, x):
+        rotation = [[1,0,0], [0, 1, 0], [0, 0, 1]]
+        return rotation
+
