a1a00afde0714373c7b327ce57e2d95a7209eea5
Lisa Julia Nebel <lisa_julia.nebel@tu-dresden.de>
2020-10-08 10:30:09 +0200

    Correct typo in the calculation of the alternator tensor

--- dune/gfe/surfacecosseratenergy.hh
+++ dune/gfe/surfacecosseratenergy.hh
@@ -245,7 +245,7 @@ RT energy(const typename Basis::LocalView& localView,
 
       for (int alpha=0; alpha<2; alpha++)
         for (int beta=0; beta<2; beta++)
-          c += sqrt(aScalar) * eps[alpha][beta] * Dune::GFE::dyadicProduct(aContravariant[alpha], aContravariant[beta]);
+          c += aScalar * eps[alpha][beta] * Dune::GFE::dyadicProduct(aContravariant[alpha], aContravariant[beta]);
 
       // Second fundamental form
       // The derivative of the normal field
