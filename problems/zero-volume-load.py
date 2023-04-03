d312cf2a9246fe924b03b1a756dd3cd2861b089b
Lisa Julia Nebel <lisa_julia.nebel@tu-dresden.de>
2023-04-03 13:45:10 +0200

    Add zero-volume-load.py

---
+++ problems/zero-volume-load.py
@@ -0,0 +1,8 @@
+import math
+
+class VolumeLoad:
+    def __init__(self, homotopyParameter):
+        self.homotopyParameter = homotopyParameter
+
+    def volumeLoad(self, x):
+        return [0,0,0]
